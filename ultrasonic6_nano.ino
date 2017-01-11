9490a68a437c5964319b93b4c08e1841c3fa27f0
JehadZC <s-gehad.aboharga@zewailcity.edu.eg>
2017-01-11 05:37:20 +0200

    Add files via upload

--- ultrasonic6_nano.ino
+++ ultrasonic6_nano.ino
@@ -9,8 +9,8 @@
 #define trigR1 4
 #define echoR1 5
 
-#define trigR2 12
-#define echoR2 13
+#define trigR2 A2
+#define echoR2 A3
 
 #define trigF 10
 #define echoF 11
@@ -20,7 +20,7 @@
 
 #define SONAR_NUM     6 // Number of sensors.
 #define MAX_DISTANCE 300 // Maximum distance (in cm) to ping.
-#define PING_INTERVAL 50 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+#define PING_INTERVAL 25 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
 
 unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
 unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
@@ -43,14 +43,14 @@ void setup() {
 }
 
 void loop() {
-  for (uint8_t i = 0; i < SONAR_NUM; i++) {                              // Loop through all the sensors.
-    if (millis() >= pingTimer[i]) {                                  // Is it this sensor's time to ping?
-      pingTimer[i] += PING_INTERVAL * SONAR_NUM;                      // Set next time this sensor will be pinged.
-      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle();   // Sensor ping cycle complete, do something with the results.
-      sonar[currentSensor].timer_stop();                               // Make sure previous timer is canceled before starting a new ping (insurance).
-      currentSensor = i;                                              // Sensor being accessed.
-      cm[currentSensor] = MAX_DISTANCE;                                  // Make distance zero in case there's no ping echo for this sensor.
-      sonar[currentSensor].ping_timer(echoCheck);                    // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
+      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
+      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
+      currentSensor = i;                          // Sensor being accessed.
+      cm[currentSensor] = MAX_DISTANCE;                      // Make distance zero in case there's no ping echo for this sensor.
+      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
     }
   }
   // Other code that *DOESN'T* analyze ping results can go here.
